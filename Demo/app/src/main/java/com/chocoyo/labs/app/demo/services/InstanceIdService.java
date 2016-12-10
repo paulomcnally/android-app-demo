916a8b48067c922611c7e6705a9018509df17494
Paulo McNally <paulomcnally@gmail.com>
2016-12-10 16:37:36 -0600

    push notification

---
+++ Demo/app/src/main/java/com/chocoyo/labs/app/demo/services/InstanceIdService.java
@@ -0,0 +1,19 @@
+package com.chocoyo.labs.app.demo.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.i("InstanceIdService", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
---
+++ Demo/app/src/main/java/com/chocoyo/labs/app/demo/services/MessagingService.java
@@ -0,0 +1,19 @@
+package com.chocoyo.labs.app.demo.services;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d("foo", "From: " + remoteMessage.getFrom());
+        Log.d("foo", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
