c13baf3a4b77cda451e62719d062e4b72e03c4b8
Paulo McNally <paulomcnally@gmail.com>
2016-12-17 13:40:02 -0600

    add permissions

--- Demo/app/src/main/java/com/chocoyo/labs/app/demo/ui/activities/MainActivity.java
+++ Demo/app/src/main/java/com/chocoyo/labs/app/demo/ui/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.chocoyo.labs.app.demo.ui.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +27,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 300;
+
+
     private FirebaseDatabase mDatabase;
 
     @BindView(R.id.open) Button buttonOpen;
@@ -54,6 +66,25 @@ public class MainActivity extends AppCompatActivity {
         // set button text
         buttonOpen.setText(getString(R.string.app_name));
 
+        validatePermission();
+
+    }
+
+    private void actionDoneExample() {
+        Log.i(TAG, "Permiso consedido");
+    }
+
+    private void validatePermission() {
+
+        int writeExternalStorage = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (writeExternalStorage == PackageManager.PERMISSION_GRANTED) {
+            actionDoneExample();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 
     @OnClick(R.id.open)
@@ -84,6 +115,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    actionDoneExample();
+                }
+            }
+        }
     }
 
     @Override
